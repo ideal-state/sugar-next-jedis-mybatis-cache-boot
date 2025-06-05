ea80aa3551905c9cbcdff67339ef4ee9a538d3a4
ketikai <ketikai@idealstate.team>
2025-06-05 21:13:35 +0800

    chore(libs,build): 将 sugar-next-mybatis-boot 和 sugar-next-jedis-boot 从快照版本更改为 0.1.0 的稳定版本，修改版本号为稳定版本，补充稳定版本发布所须的 pom 属性
    [skip ci]
    
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "team.idealstate.sugar.boot"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 java {
     toolchain {
@@ -55,6 +55,32 @@ dependencies {
 publishing {
     repositories {
         project(project)
+    }
+    publications {
+        main {
+            pom {
+                description.set("Coffee(Java) with sugar is sweeter.")
+                url.set("https://github.com/ideal-state/sugar-next-jedis-mybatis-cache-boot")
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/ideal-state/sugar-next-jedis-mybatis-cache-boot")
+                    connection.set("scm:git:https://github.com/ideal-state/sugar-next-jedis-mybatis-cache-boot.git")
+                    developerConnection.set("scm:git:https://github.com/ideal-state/sugar-next-jedis-mybatis-cache-boot.git")
+                }
+                developers {
+                    developer {
+                        id.set("ideal-state")
+                        name.set("ideal-state")
+                        email.set("support@idealstate.team")
+                    }
+                }
+            }
+        }
     }
 }
 
